a9e4e9f762b7f3ef0f3e9ecc957a4abe841b5fc0
dkole13 <dasha10206@gmail.com>
2026-02-03 00:54:21 +0300

    embedder updated, added faiss storage; requirements.txt updated

--- embedder.py
+++ embedder.py
@@ -1,7 +1,10 @@
 import faiss
 import torch
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from sentence_transformers import SentenceTransformer
 
 
 class Embedder:
@@ -39,15 +42,19 @@ class Embedder:
         self._child_chunk_size = child_chunk_size
         self._parent_chunk_overlap = parent_chunk_overlap
         self._child_chunk_overlap = child_chunk_overlap
-        self._model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")  # TODO: larger model
+        self._model = HuggingFaceEmbeddings(
+            model_name="Qwen/Qwen3-Embedding-0.6B",  # TODO: larger model
+            model_kwargs={"device": self._device}
+        )
+        self._storage = self._init_storage()
 
-    def tokenize(
+    def add_documents(
             self,
             texts: list[str]
     ):
         """
-        Splits texts into chunks.
-        :param texts:
+        Tokenizes, embeds texts and adds to FAISS.
+        :param texts: Texts to process.
         :return:
         """
         parent_splitter = RecursiveCharacterTextSplitter(
@@ -63,31 +70,64 @@ class Embedder:
                         "\n\n", "\n", ".", "?", "!", " ", ""]
         )
 
-        # a list[list[str]], where each list[str] is a document, and str is a chunk
         documents_chunks = [parent_splitter.split_text(text) for text in texts]
-        child_chunks = []
+        docs = []
 
         for document_id, document_chunks in enumerate(documents_chunks):
             for parent_id, parent_chunk in enumerate(document_chunks):
                 children_chunks = child_splitter.split_text(parent_chunk)
                 for child_id, child_chunk in enumerate(children_chunks):
-                    child_embedding = self._embed_document(child_chunk)
                     metadata = {
                         "document_id": document_id,
                         "parent_id": parent_id,
-                        "chunk_id": child_id,
-                        "embedding": child_embedding
+                        "chunk_id": child_id
                     }
+                    child_doc = Document(child_chunk, metadata=metadata)
+                    docs.append(child_doc)
 
-    def _embed_document(self, chunk: str):
-        return self._model.encode_document(chunk, device=self._device)
+        self._storage.add_documents(docs)
 
-    def embed_query(self, query: str):
-        return self._model.encode_query(query, device=self._device)
+    def embed(self, chunk: str):
+        return self._model.encode(chunk, device=self._device)
+
+    def _init_storage(
+            self,
+            # TODO: 1024 is for 0.6B model, change according to the model we use later
+            model_dimension: int = 1024
+    ):
+        """
+        Initializes FAISS vector storage for similarity search.
+        :param model_dimension: Embedding vector size.
+        :return: Vector storage.
+        """
+        index = faiss.IndexFlatL2(model_dimension)
+        vector_store = FAISS(
+            embedding_function=self._model,
+            index=index,
+            docstore=InMemoryDocstore(),
+            index_to_docstore_id={},
+        )
+        return vector_store
+
+    def similarity_search(
+            self,
+            query: str,
+            k=5
+    ):
+        """
+        Performs similarity search on a given query.
+        :param query:
+        :param k: How many results should be returned.
+        :return: List of most similar documents.
+        """
+        return self._storage.similarity_search(query, k)
 
 
-em = Embedder(parent_chunk_size=5, parent_chunk_overlap=3, child_chunk_size=3,
+# usage example for later:
+"""em = Embedder(parent_chunk_size=5, parent_chunk_overlap=3, child_chunk_size=3,
               child_chunk_overlap=1)
-print(em.tokenize(["# this is a sample text."
-                   "### subsection.",
-                   "the. quick? brown! fox jumps"]))
+em.add_documents(["# this is a sample text."
+                  "### subsection.",
+                  "the. quick? brown! fox jumps"])
+
+print(em.similarity_search("fox"))"""
