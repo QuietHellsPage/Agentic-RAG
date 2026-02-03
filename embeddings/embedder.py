e41d695b56c55bc09d4651e33dddfc8278375c0d
dkole13 <dasha10206@gmail.com>
2026-02-03 16:34:02 +0300

    Initial version of embeddings processor (#14)

---
+++ embeddings/embedder.py
@@ -0,0 +1,147 @@
+"""
+Module to operate processing of raw text and saving it to vector db
+"""
+
+import faiss
+import torch
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from embeddings.models import EmbedderConfig
+
+
+class Embedder:
+    """
+    Instance for all operations with embeddings
+    """
+
+    def __init__(self, config: EmbedderConfig, device: str | None = None):
+        """
+        Embedding model wrapper.
+        :param parent_chunk_size: Number of tokens in a parent chunk.
+        :param config: Configuration for embedder
+        :param device: Device used for computation.
+        """
+
+        if device is None:
+            self._device = "cuda" if torch.cuda.is_available() else "cpu"
+        else:
+            self._device = device
+        self._parent_chunk_size = config.parent_chunk_size
+        self._child_chunk_size = config.child_chunk_size
+        self._parent_chunk_overlap = config.parent_chunk_overlap
+        self._child_chunk_overlap = config.child_chunk_overlap
+        self._model = HuggingFaceEmbeddings(
+            model_name="Qwen/Qwen3-Embedding-0.6B",  # TODO: larger model # pylint: disable=fixme
+            model_kwargs={"device": self._device},
+        )
+        self._storage = self._init_storage()
+
+    def add_documents(self, texts: list[str]):
+        """
+        Tokenizes, embeds texts and adds to FAISS.
+        :param texts: Texts to process.
+        :return:
+        """
+        parent_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self._parent_chunk_size,
+            chunk_overlap=self._parent_chunk_overlap,
+            separators=[
+                "\n#",
+                "\n##",
+                "\n###",
+                "\n####",
+                "\n#####",
+                "\n######",
+                "\n\n",
+                "\n",
+                ".",
+                "?",
+                "!",
+                " ",
+                "",
+            ],
+        )
+        child_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self._child_chunk_size,
+            chunk_overlap=self._child_chunk_overlap,
+            separators=[
+                "\n#",
+                "\n##",
+                "\n###",
+                "\n####",
+                "\n#####",
+                "\n######",
+                "\n\n",
+                "\n",
+                ".",
+                "?",
+                "!",
+                " ",
+                "",
+            ],
+        )
+
+        documents_chunks = [parent_splitter.split_text(text) for text in texts]
+        docs = []
+
+        for document_id, document_chunks in enumerate(documents_chunks):
+            for parent_id, parent_chunk in enumerate(document_chunks):
+                children_chunks = child_splitter.split_text(parent_chunk)
+                for child_id, child_chunk in enumerate(children_chunks):
+                    metadata = {
+                        "document_id": document_id,
+                        "parent_id": parent_id,
+                        "chunk_id": child_id,
+                    }
+                    child_doc = Document(child_chunk, metadata=metadata)
+                    docs.append(child_doc)
+
+        self._storage.add_documents(docs)
+
+    def embed(self, chunk: str):
+        """
+        Embed single chunk
+        """
+        return self._model.encode(chunk, device=self._device)  # type: ignore[attr-defined]
+
+    def _init_storage(
+        self,
+        # TODO: 1024 is for 0.6B model, change according to the model we use later # pylint: disable=fixme
+        model_dimension: int = 1024,
+    ):
+        """
+        Initializes FAISS vector storage for similarity search.
+        :param model_dimension: Embedding vector size.
+        :return: Vector storage.
+        """
+        index = faiss.IndexFlatL2(model_dimension)
+        vector_store = FAISS(
+            embedding_function=self._model,
+            index=index,
+            docstore=InMemoryDocstore(),
+            index_to_docstore_id={},
+        )
+        return vector_store
+
+    def similarity_search(self, query: str, k=5):
+        """
+        Performs similarity search on a given query.
+        :param query:
+        :param k: How many results should be returned.
+        :return: List of most similar documents.
+        """
+        return self._storage.similarity_search(query, k)
+
+
+## usage example for later:
+# em = Embedder(parent_chunk_size=5, parent_chunk_overlap=3, child_chunk_size=3,
+#               child_chunk_overlap=1)
+# em.add_documents(["# this is a sample text."
+#                   "### subsection.",
+#                   "the. quick? brown! fox jumps"])
+
+# print(em.similarity_search("fox"))
---
+++ embeddings/models.py
@@ -0,0 +1,33 @@
+"""
+Models for processing embeddings
+"""
+
+from typing import Annotated
+
+from pydantic import BaseModel, Field, model_validator
+
+
+class EmbedderConfig(BaseModel):
+    """
+    Storage for embeddings processing
+    """
+
+    parent_chunk_size: Annotated[int, Field(default=4096, gt=0)]
+    parent_chunk_overlap: Annotated[int, Field(default=400, gt=0)]
+    child_chunk_size: Annotated[int, Field(default=1024, gt=0)]
+    child_chunk_overlap: Annotated[int, Field(default=200, gt=0)]
+
+    @model_validator(mode="after")
+    def validate_fields(self):
+        """
+        Validate fields of model
+        """
+        if self.child_chunk_overlap >= self.child_chunk_size:
+            raise ValueError(
+                "Child chunk overlap can not be bigger or the same as child chunks"
+            )
+
+        if self.parent_chunk_overlap >= self.parent_chunk_size:
+            raise ValueError(
+                "Parent chunk overlap can not be bigger or the same as parent chunks"
+            )
